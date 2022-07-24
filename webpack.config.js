afce34e62bcc2ecf2eb11237c8d746dc91b89666
kongcodes <78190504+kongcodes@users.noreply.github.com>
2022-07-24 21:25:41 +0800

    多入口配置

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,17 @@
 const { resolve } = require("path");
 module.exports = {
 	// 入口
-	entry: "./src/index.js",
+	// entry: "./src/index.js",
+	entry: {
+		index: "./src/index.js",
+		login: "./src/login.js",
+	},
 	// 出口
 	output: {
 		// 生成资源存放的位置；path 必须是绝对路径，默认是 dist目录
 		path: resolve(__dirname, "./build"),
 		// 生成的资源叫什么
-		filename: "index.js",
+		filename: "[name].js",
 	},
 	mode: "development",
 };
