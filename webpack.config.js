64b7a1a2636224bc3e5768885c1c339630978b49
kongcodes <78190504+kongcodes@users.noreply.github.com>
2022-08-11 22:27:42 +0800

    图片压缩

--- webpack.config.js
+++ webpack.config.js
@@ -1,48 +1,48 @@
-const { resolve } = require("path");
-const htmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const minicssPlugin = require("mini-css-extract-plugin");
+const { resolve } = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const minicssPlugin = require('mini-css-extract-plugin')
 module.exports = {
 	// 入口
-	entry: { index: "./src/index.js" },
+	entry: { index: './src/index.js' },
 	// 出口
 	output: {
 		// 生成资源存放的位置；path 必须是绝对路径，默认是 dist目录
-		path: resolve(__dirname, "./dist"),
+		path: resolve(__dirname, './dist'),
 		// 生成的资源叫什么
-		filename: "[name].js",
+		filename: '[name].js',
 	},
-	mode: "development",
+	mode: 'development',
 	plugins: [
 		new CleanWebpackPlugin(),
 		new minicssPlugin({
 			// filename: "index.css",
 			// 将css文件放到style目录下
-			filename: "style/index.css",
+			filename: 'style/index.css',
 		}),
 		new htmlWebpackPlugin({
-			template: "./public/index.html",
-			filename: "index.html",
-			chunks: ["index"],
+			template: './public/index.html',
+			filename: 'index.html',
+			chunks: ['index'],
 		}),
 	],
 	resolveLoader: {
-		modules: ["node_modules", "./myLoaders"],
+		modules: ['node_modules', './myLoaders'],
 	},
 	module: {
 		rules: [
 			{
 				test: /\.css$/,
 				// use: ["style-loader", "css-loader"],
-				use: [minicssPlugin.loader, "css-loader"],
+				use: [minicssPlugin.loader, 'css-loader'],
 			},
 			{
 				test: /\.less$/,
 				use: [
 					minicssPlugin.loader,
-					"css-loader",
-					"postcss-loader",
-					"less-loader",
+					'css-loader',
+					'postcss-loader',
+					'less-loader',
 					// 替换成自己手写实现的loader
 					// "mini-style-loader",
 					// "mini-css-loader",
@@ -55,12 +55,12 @@ module.exports = {
 				use: [
 					{
 						// loader: resolve(__dirname, "./myLoaders/k-loader.js"),
-						loader: "k-loader",
-						options: { title: "你好" },
+						loader: 'k-loader',
+						options: { title: '你好' },
 					},
 					{
 						// loader: resolve(__dirname, "./myLoaders/l-loader.js"),
-						loader: "l-loader",
+						loader: 'l-loader',
 					},
 				],
 			},
@@ -79,16 +79,19 @@ module.exports = {
 			{
 				test: /\.(jpg|png|gif|webp)$/,
 				// use: "file-loader",
-				use: {
-					loader: "url-loader",
-					options: {
-						name: "[name].[ext]",
-						outputPath: "images", // 图片资源存放目录
-						publicPath: "../images", // 图片资源引入位置
-						esModule: false,
-						limit: 3 * 1024, // 单位：字节
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							name: '[name].[ext]',
+							outputPath: 'images', // 图片资源存放目录
+							publicPath: '../images', // 图片资源引入位置
+							esModule: false,
+							limit: 3 * 1024, // 单位：字节
+						},
 					},
-				},
+					'image-webpack-loader',
+				],
 			},
 			// {
 			// 	test: /\.html$/,
@@ -96,4 +99,4 @@ module.exports = {
 			// },
 		],
 	},
-};
+}
