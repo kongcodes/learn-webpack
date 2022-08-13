448538426d1e87191676aaa83f8404d8d295ec0c
kongcodes <78190504+kongcodes@users.noreply.github.com>
2022-08-13 23:46:06 +0800

    字体处理和多页面应用的通用方案

--- mpa.config.js
+++ mpa.config.js
@@ -1,21 +1,48 @@
-const { resolve } = require('path')
+const { resolve, join } = require('path')
 const htmlWebpackPlugin = require('html-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const minicssPlugin = require('mini-css-extract-plugin')
+const glob = require('glob')
+
+const setMPA = () => {
+	const entry = {}
+	const htmlwebpackplugins = []
+
+	// 查询页面入口文件 及 相应的html模板
+	// 提取页面入口名称，用于entry及chunkName
+	// new htmlWebpackPlugin
+	const entryPath = glob.sync('./src/*/index.js')
+	console.log('entryPath----', entryPath) // ['./src/index/index.js', './src/list/index.js' ...]
+	entryPath.map((item) => {
+		const entryName = item.match(/src\/(.*)\/index\.js$/)[1]
+		console.log(entryName) // index list login
+		entry[entryName] = item
+		// console.log('entry-----', entry)
+		htmlwebpackplugins.push(
+			// 在这里实例化是没有意义的，最终还要放在 plugins 中展开
+			new htmlWebpackPlugin({
+				template: join(__dirname, `./src/${entryName}/index.html`), // 使用 join 拼接成绝对路径
+				filename: `${entryName}.html`,
+				chunks: [entryName],
+			})
+		)
+	})
+
+	return {
+		entry,
+		htmlwebpackplugins,
+	}
+}
+
+const { entry, htmlwebpackplugins } = setMPA()
+
 module.exports = {
-	// 入口
-	entry: {
-		index: './src/index.js',
-		detail: './src/detail.js', // 新增detail入口
-	},
-	// 出口
-	output: {
-		// 生成资源存放的位置；path 必须是绝对路径，默认是 dist目录
-		path: resolve(__dirname, './dist'),
-		// 生成的资源叫什么
-		filename: '[name].js',
-	},
 	mode: 'development',
+	entry,
+	output: {
+		path: resolve(__dirname, './mpa'),
+		filename: 'js/[name].js',
+	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new minicssPlugin({
@@ -23,16 +50,7 @@ module.exports = {
 			// 将css文件放到style目录下
 			filename: 'style/index.css',
 		}),
-		new htmlWebpackPlugin({
-			template: './public/index.html',
-			filename: 'index.html',
-			chunks: ['index'],
-		}),
-		new htmlWebpackPlugin({
-			template: './public/detail.html',
-			filename: 'detail.html',
-			chunks: ['detail'],
-		}),
+		...htmlwebpackplugins,
 	],
 	resolveLoader: {
 		modules: ['node_modules', './myLoaders'],
